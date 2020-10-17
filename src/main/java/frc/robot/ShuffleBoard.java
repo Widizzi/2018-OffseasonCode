827c727ed6255f7360d98a903064a6f9f48f4d63
Widizzi <florian@widi.ch>
2020-10-17 14:01:16 +0200

    ShuffleBoard
    adds a speed tab to the shuffleboard for debugging

---
+++ src/main/java/frc/robot/ShuffleBoard.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+public class ShuffleBoard {
+
+    private static ShuffleboardTab speeds = Shuffleboard.getTab("Speeds");
+
+    public void updateSpeeds() {
+
+    }
+
+}
